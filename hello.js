9616fe0f015add9ed6e932e2d31aea00c47eff7e
TanerOezdogan <taner.oezdogan@eu.rhenus.com>
2023-06-21 10:28:14 +0200

    hihihi

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
 const container = document.createElement("div");
+container.classList.add("container");
 
-container.classList.add("container")
+const button = document.querySelector("button");
+button.addEventListener("click", createNewGrid);
+
+createDivs(16);
+
+
 function createDivs(number) {
     if(number <= 100) {                                                                                                     //limit the number to save resources
         for(let i = 1; i <= number; i++) {                                                                         //create a column + add class "column"
@@ -18,9 +24,6 @@ function createDivs(number) {
     const cells = document.querySelectorAll(".innerDivs");                                     //select all elements containing class "innerDivs" + addEventListener to color each div on mousenter
     cells.forEach(cell=>cell.addEventListener("mouseenter", addClassName));
 }
-createDivs(16);
-
-
 
 function addClassName() {                                                                                             //callback function for Eventlistener to add and remove class
     if(!this.classList.contains("mouseover")) {
@@ -31,11 +34,8 @@ function addClassName() {                                                       
 
 }
 
-const button = document.querySelector("button")
-
 function createNewGrid() {                                                                                            //function for the button to create a new grid
-    let gridSize = prompt("?^2")
+    let gridSize = prompt("?^2");
     document.querySelectorAll(".innerDivs").forEach(e => e.remove());
     createDivs(gridSize);
 }
-button.addEventListener("click", createNewGrid);
